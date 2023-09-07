1c7717845e232e37cb0f04317987eba15d648392
Michael Muse <michaelmuse@gmail.com>
2023-09-07 13:14:58 -0500

    Merge current_files branch into master

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,10 @@ const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
   ...jestConfig,
-  // Explicitly set the test environment to "jsdom"
   testEnvironment: "jsdom",
-  preset: "@lwc/jest-preset", // Merged from file 2
+  preset: "@lwc/jest-preset",
   moduleNameMapper: {
-    "^c/(.*)$": "<rootDir>/force-app/main/default/lwc/$1", // Merged from file 2
-    // Add other mappings here if needed
+    "^c/(.*)$": "<rootDir>/force-app/main/default/lwc/$1",
   },
   testPathIgnorePatterns: ["/node_modules/", "/.history/"],
   modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
