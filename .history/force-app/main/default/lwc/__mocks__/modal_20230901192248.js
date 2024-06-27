49642f20a9280063192d351fdbc9e6c3af5a51c3
Anthony Sanchez <anthony.sanchez@vallynt.com>
2024-06-26 22:16:00 -0700

    Remove .history folder

--- .history/force-app/main/default/lwc/__mocks__/modal_20230901192248.js
+++
@@ -1,5 +0,0 @@
-// __mocks__/modal.js
-import { LightningElement } from "lwc";
-export default class Modal extends LightningElement {
-  // Mock implementation here
-}
--- .history/force-app/main/default/lwc/flowdometerInstructions/__mocks__/lwc_20230904073435.js
+++
@@ -1,4 +0,0 @@
-//force-app/main/default/lwc/flowdometerInstructions/__mocks__/lwc.js
-export default class LightningElement {
-  // put your mock methods and properties here
-}
